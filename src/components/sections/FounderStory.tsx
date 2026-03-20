c5afd868553838f25307ae7ccd2d1aa3584d2eef
felixodette <felix.odette@gmail.com>
2026-03-20 18:59:19 +0300

    Founder story changes

--- src/components/sections/FounderStory.tsx
+++ src/components/sections/FounderStory.tsx
@@ -15,7 +15,7 @@ const FounderStory = () => (
           viewport={{ once: true }}
           transition={{ duration: 0.6 }}
         >
-          <img src="/images/about-img.jpg" alt="Founder" className="rounded-lg shadow-xl w-full object-cover" />
+          <img src="/images/about-img.png" alt="Founder" className="rounded-lg shadow-xl w-full object-cover" />
         </motion.div>
         <motion.div
           initial={{ opacity: 0, x: 40 }}
